2ccb6b5cddab8ad4bbb7b6e0d1c8f818360f2f0b
shakil <129603316+shakilAhamed001@users.noreply.github.com>
2026-02-16 11:16:24 +0600

    logical operator

---
+++ operator/logical.py
@@ -0,0 +1,13 @@
+"""logical operators are used to combine conditional statements. The three logical operators in Python are and, or, and not.
+1. and: Returns True if both statements are true.
+2. or: Returns True if at least one of the statements is true.
+3. not: Reverses the result, returns False if the result is true and vice versa."""
+
+a = 10
+b = 5
+
+print("a > 5 and b < 10:", a > 5 and b < 10)  # Output: True
+print("a > 5 or b > 10:", a > 5 or b > 10)    # Output: True
+print("not (a > 5):", not (a > 5))            # Output: False
+print("not (b < 10):", not (b < 10))          # Output: False   
+
